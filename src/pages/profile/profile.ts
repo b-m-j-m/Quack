b4f4c7cc24da49e4569f89bd7f75bc52f6de6e5e
kilian <kilian@4xschulte.de>
2018-11-18 09:58:22 +0100

    profile page

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 /**
  * Generated class for the ProfilePage page.
@@ -15,7 +17,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public user = {};
+  constructor(public navCtrl: NavController, public navParams: NavParams, afAuth: AngularFireAuth, public db: AngularFirestore) {
+
+    db.collection("users").doc(afAuth.auth.currentUser.uid).get()
+      .subscribe(snapshot => {
+        let data = snapshot.data();
+        this.user = {
+          id: snapshot.id,
+          name: data.name,
+          profile: data.profile.length > 1 ? data.profile : 'assets/imgs/avatar-horst.jpg'
+        }
+      });
+
   }
 
   ionViewDidLoad() {
